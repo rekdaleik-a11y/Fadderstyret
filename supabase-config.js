0ded7b31a0e6d6e607abc372b157b7115c2c5d1d
rekdaleik-a11y <rekdaleik@gmail.com>
2026-03-29 21:54:54 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.FADDERPLAN_SUPABASE_CONFIG = {
   url: "https://gctkyzqazyqynsuushed.supabase.co",
-  anonKey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjdGt5enFhenlxeW5zdXVzaGVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3OTkyNDQsImV4cCI6MjA5MDM3NTI0NH0.FNPwaHaeXaSorz0X6ge776N-8fKb66nob_EbgXatPB8,
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjdGt5enFhenlxeW5zdXVzaGVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3OTkyNDQsImV4cCI6MjA5MDM3NTI0NH0.FNPwaHaeXaSorz0X6ge776N-8fKb66nob_EbgXatPB8",
   boardId: "fadderplan-2026"
 };
